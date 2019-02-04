d183beefd10f6d1ddd62c8d39e3aac5a7d27546f
Peter Turner <syntax.valid@gmail.com>
2019-02-04 19:33:35 +0000

    Add config to the reader

--- cmd/csvreader/main.go
+++ cmd/csvreader/main.go
@@ -1,5 +1,36 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/tkanos/gonfig"
+)
+
+var ()
+
+func init() {
+	fmt.Println("Initialising reader")
+	cfg := Config{}
+
+	// wd, _ := os.Getwd()
+	err := gonfig.GetConf("./dev.json", &cfg)
+	if err != nil {
+		fmt.Printf("Could not load configuration! %v\n", err)
+		panic("Aborting...")
+	}
+
+	fmt.Printf("Using config: %+v\n", cfg)
+}
+
+// Config holds the configuration for reader and the endpoint to store the data
+type Config struct {
+	Database struct {
+		Host string
+		Port string
+	}
+	Separator string
+}
+
 func main() {
 
 }
